232418d55c2b3de006772de77296c7d12fa9deac
John Kooker <jkooker@gmail.com>
2010-04-20 19:35:57 -0700

    add touch handling and display.

---
+++ OptiActor/OATouchesView.h
@@ -0,0 +1,16 @@
+//
+//  OATouchesView.h
+//  OptiActor
+//
+//  Created by John Kooker on 4/20/10.
+//  Copyright 2010 John Kooker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface OATouchesView : UIView {
+    CGPoint touchPoint;
+}
+
+@end
---
+++ OptiActor/OATouchesView.m
@@ -0,0 +1,88 @@
+//
+//  OATouchesView.m
+//  OptiActor
+//
+//  Created by John Kooker on 4/20/10.
+//  Copyright 2010 John Kooker. All rights reserved.
+//
+
+#import "OATouchesView.h"
+
+
+@implementation OATouchesView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        // Initialization code
+        touchPoint.x = -50;
+        touchPoint.y = -50;
+    }
+    return self;
+}
+
+#define CIRCLE_RADIUS 40
+
+- (void)drawRect:(CGRect)rect {
+    if (touchPoint.x > 0) {
+        CGContextRef contextRef = UIGraphicsGetCurrentContext();
+        CGContextSetRGBFillColor(contextRef, 0, 0, 0.4, 1);
+        CGContextSetRGBStrokeColor(contextRef, 0, 0, 1, 1);
+        CGContextSetLineWidth(contextRef, 3);
+        
+        CGRect circle_rect = CGRectMake(touchPoint.x - CIRCLE_RADIUS, touchPoint.y - CIRCLE_RADIUS, 2 * CIRCLE_RADIUS, 2 * CIRCLE_RADIUS);
+        
+        // Draw a circle (filled)
+        CGContextFillEllipseInRect(contextRef, circle_rect);
+        // Draw a circle (border only)
+        CGContextStrokeEllipseInRect(contextRef, circle_rect);
+    }
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+#pragma mark Touch Handling
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        // monitor and log it
+        touchPoint.x = [touch locationInView:self].x;
+        touchPoint.y = [touch locationInView:self].y;
+        [self setNeedsDisplay];
+        
+        NSLog(@"new touch at %@", NSStringFromCGPoint([touch locationInView:self]));
+    }
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        // monitor and log it
+        touchPoint.x = [touch locationInView:self].x;
+        touchPoint.y = [touch locationInView:self].y;
+        [self setNeedsDisplay];
+        
+        NSLog(@"touch moved to %@", NSStringFromCGPoint([touch locationInView:self]));
+    }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        // redraw and log it
+        touchPoint.x = -50;
+        touchPoint.y = -50;
+        [self setNeedsDisplay];
+
+        NSLog(@"touch ended at %@", NSStringFromCGPoint([touch locationInView:self]));
+    }
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"touches cancelled");
+}
+
+
+
+
+@end
