f46c825ed546e53af7f22731109e296e91b7ff4d
John Kooker <jkooker@gmail.com>
2010-05-05 00:13:50 -0700

    implement selection.

--- OAInfoViewController.m
+++ OAInfoViewController.m
@@ -241,14 +241,15 @@ enum OASection {
 #pragma mark Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Navigation logic may go here. Create and push another view controller.
-	/*
-	 <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-	 [self.navigationController pushViewController:detailViewController animated:YES];
-	 [detailViewController release];
-	 */
+    if (indexPath.section == OASectionTouchInterface) {
+        [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:touchProcessingType inSection:indexPath.section]].accessoryType = UITableViewCellAccessoryNone;
+        
+        touchProcessingType = indexPath.row;
+        
+        [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryCheckmark;
+        
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    }
 }
 
 
